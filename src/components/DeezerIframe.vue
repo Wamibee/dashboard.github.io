31ac89e3a7293b65febfa45ebe027d851a0b1fa7
Aimee RITLENG <aimee.ritleng@soprasteria.com>
2025-12-08 14:30:31 +0100

    feat: KISS

--- src/components/DeezerIframe.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/playlist/13888012901" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
-</template>
-
-<script>
-</script>
